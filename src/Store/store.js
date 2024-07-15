5c49dbc26e5edda9d3eb5e3f66148a1d4ca2e96c
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-07-15 11:45:14 +0530

    Delete unused Redux slices

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,31 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
-import jobPostReducer from './allJobPost';
 import jobPostDetailReducer from './singleJobPost';
-import resultPostReducer from './allResultPost';
 import resultPostDetailsReducer from './singleResultPost';
-import admitCardReducer from './allAdmitCardPost';
 import admitCardDetailsReducer from './singleAdmitCardPost';
-import answerKeyPostReducer from './allAnswerKeyPost';
 import answerKeyDetailsReducer from './singleAnswerKeyPost';
-import admissionPostReducer from './allAdmissionPost';
 import admissionDetailsReducer from './singleAdmissionPost';
-import upcommingPostReducer from './allUpcomminPost';
 
 
 
 const store = configureStore({
   reducer: {
-    jobPost: jobPostReducer,
     jobPostDetails: jobPostDetailReducer,
-    resultPost: resultPostReducer,
     resultPostDetails: resultPostDetailsReducer,
-    admitCard: admitCardReducer,
     admitCardDetails: admitCardDetailsReducer,
-    answerKeyPost: answerKeyPostReducer,
     answerKeyDetails: answerKeyDetailsReducer,
-    admissionPost: admissionPostReducer,
     admissionDetails: admissionDetailsReducer,
-    upcommingPost: upcommingPostReducer,
   },
 });
 

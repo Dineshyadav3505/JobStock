842bf6c70d48c8f02a34d6113d6e283b258fc697
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-07-27 12:20:32 +0530

    Add new SVG icons

--- src/Components/Admin/SideNavbar.jsx
+++ src/Components/Admin/SideNavbar.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import Cookies from 'js-cookie';
 
 const SideNavbar = () => {
     const link =[
-        {label: 'Job Post',          to: '/jobPost',     img:"Images/ajob.svg"  },
-        {label: 'State Post',        to: '/stateJob',    img:"Images/astate.svg"  },
-        {label: 'Result Post',       to: '/resulJob',    img:"Images/aresult.svg"  },
-        {label: 'AdmitCard Post',    to: '/admitCard',   img:"Images/aadmit.svg"  },
-        {label: 'AnswerKey Post',    to: '/answerKey',   img:"Images/aanswer.svg"  },
-        {label: 'UpComming Post',    to: '/upComming',   img:"Images/aupcomming.svg"  }
-  
+        {label: 'Job Post',          to: '/jobPost',         img:"Images/ajob.svg"  },
+        {label: 'State Post',        to: '/stateJob',        img:"Images/astate.svg"  },
+        {label: 'Result Post',       to: '/resulJob',        img:"Images/aresult.svg"  },
+        {label: 'AdmitCard Post',    to: '/admitCardPost',   img:"Images/aadmit.svg"  },
+        {label: 'AnswerKey Post',    to: '/answerKeyPost',   img:"Images/aanswer.svg"  },
+        {label: 'Admission Post',    to: '/admissionPost',   img:"Images/aadmission.svg"  },
+        {label: 'UpComming Post',    to: '/upCommingPost',   img:"Images/aupcomming.svg"  },
     ]
+
+    const logout = ()=>{
+        Cookies.remove('cb_chec');
+    }
+
   return (
     <div className={`hidden lg:flex flex-col text-white h-full w-[6%]`}>
         {link.map((item,index)=>(
-            <NavLink to={item.to} className="px-2 py-2 mb-2 flex flex-col items-center rounded hover:bg-zinc-800">
+            <NavLink key={index} to={item.to} className="px-2 py-2 mb-2 flex flex-col items-center rounded hover:bg-zinc-800">
                 <img className='h-5' src={item.img} alt="" />
                 <p className='text-xs mt-2 text-center'>{item.label}</p>
             </NavLink>
         ))}
+        <NavLink onClick={logout} to="/" className="px-2 py-2 mb-2 flex flex-col items-center rounded hover:bg-zinc-800">
+            <img className='h-5' src="Images/alogout.svg" alt="" />
+            <p className='text-xs mt-2 text-center'>LogOut</p>
+        </NavLink>
+
     </div>
   )
 }
